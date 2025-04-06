4c417000dffb6b91975078b10e8baa885c3184db
Vittoria <vittoriaalataazzurra@gmail.com>
2025-04-06 22:16:51 +0200

    Commento Iniziale

---
+++ src/main/java/com/github/vittoriaalata/effettiterminale/Animazioni.java
@@ -0,0 +1,12 @@
+package com.github.vittoriaalata.effettiterminale;
+
+public enum Animazioni {
+    ANIMAZIONI;
+
+    public static int CaricamentoPuntini = 3;
+    public static String AnimazionePuntini = "... ... ... ...";
+    public static int CaricamentoSbarra = 1;
+    public static String AnimazioneSbarra = "\\/";
+    public static int CaricamentoBarra = 12;
+    public static String AnimazioneBarra = "[..........][%.........][%%........][%%%.......][%%%%......][%%%%%.....][%%%%%%....][%%%%%%%...][%%%%%%%...][%%%%%%%%..][%%%%%%%%%.][%%%%%%%%%%]";
+}
---
+++ src/main/java/com/github/vittoriaalata/effettiterminale/ScrollingText.java
@@ -0,0 +1,23 @@
+package com.github.vittoriaalata.effettiterminale;
+
+import static com.github.vittoriaalata.effettiterminale.Animazioni.*;
+
+public class ScrollingText {
+    public static void main(String[] args) throws InterruptedException {
+        ReAnimazione(CaricamentoBarra, AnimazioneBarra, 500, 100);
+    }
+
+    private static void ReAnimazione(int LunghezzaCarattereAnimato, String Animazione, int MillisAnimDelay, int Ripetizioni) throws InterruptedException {
+        for (int i = 0; i < Ripetizioni; i++) {
+                AnimazioneCaricamento(LunghezzaCarattereAnimato, Animazione, MillisAnimDelay);
+        }
+    }
+
+    public static void AnimazioneCaricamento(int LunghezzaCarattereAnimato, String Animazione, int MillisAnimDelay) throws InterruptedException {
+        while (Animazione.length() > 0) {
+            System.out.print("\r" + Animazione.substring(0, LunghezzaCarattereAnimato));
+            Animazione = Animazione.substring(LunghezzaCarattereAnimato);
+            Thread.sleep(MillisAnimDelay);
+        }
+    }
+}
